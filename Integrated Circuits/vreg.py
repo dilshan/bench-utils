cc968071f34713dd9e18238bcb1601db33aba766
dilshan <jayakody2000lk@gmail.com>
2019-09-27 21:16:24 +0530

    Add LM2596-ADJ basic calculator.

--- Integrated Circuits/vreg.py
+++ Integrated Circuits/vreg.py
@@ -22,3 +22,16 @@ def lm317_vout(r1, r2):
     print(str(1.25 * (1 + (r2/r1))) + "V")
 
 
+    
+def lm2596_vout(r1, r2):
+    if common_util.is_float_number(r1):
+        r1 = float(r1)
+    else:
+        r1 = common_util.decode_resistor_value(r1)
+
+    if common_util.is_float_number(r2):
+        r2 = float(r2)
+    else:
+        r2 = common_util.decode_resistor_value(r2)
+
+    print(str(1.23 * (1 + (r2/r1))) + "V")
